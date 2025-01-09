6c5801def696194ae7b6feba0c6974a389b23023
Beri28 <cletusberinyuy@gmail.com>
2025-01-09 10:19:35 +0100

    adjusted error in function so as to handle all test cases

--- Day-4/Day4.ts
+++ Day-4/Day4.ts
@@ -2,7 +2,7 @@ function lonelyInteger(numbers:number[]):number{
     let frequency:number[]=[]
     let temp:number[]=[]
     for(let i=0;i<numbers.length;i++){
-        frequency=[...numbers.slice(i+1),...numbers.slice(0,i-1)]
+        frequency=i!==0?[...numbers.slice(i+1),...numbers.slice(0,i)]:[...numbers.slice(i+1)]
         if(!frequency.includes(numbers[i])){
             temp.push(numbers[i])
         }
@@ -10,3 +10,5 @@ function lonelyInteger(numbers:number[]):number{
     return temp[0]
 }
 console.log(lonelyInteger([1,2,3,4,3,2,1]))
+console.log(lonelyInteger([1,1,2]))
+console.log(lonelyInteger([0,0,1,2,1]))
