e434c611f51b9852159e3d7d2dcc3f11524d25a5
Beri28 <cletusberinyuy@gmail.com>
2025-01-12 16:45:53 +0100

    Minor modifications to function and addition of readme file

--- Day-7/Day7.ts
+++ Day-7/Day7.ts
@@ -1,17 +1,14 @@
-const diagonalDifference=(matrix:number[][])=>{
+const diagonalDifference=(matrix:number[][]):number=>{
     let leftDiagonal:number=0
     let rightDiagonal:number=0
     for(let i=0;i<matrix[0].length;i++){
-        // for(let j=i;j<matrix[0].length;j++){
-        //     console.log(matrix[i][j])
-        // }
         console.log(matrix[i])
         leftDiagonal+=matrix[i][i]
         rightDiagonal+=matrix[i][matrix[0].length-1-i]
-        // console.log(matrix[i][matrix[0].length-1-i])
     }
     console.log(Math.abs(leftDiagonal-rightDiagonal))
-    // console.log(matrix[0].length)
+    return Math.abs(leftDiagonal-rightDiagonal)
 }
 diagonalDifference([[1,2,3],[4,5,6],[7,8,9]])
 diagonalDifference([[1,2,3],[4,5,6],[9,8,9]])
+diagonalDifference([[11,2,4],[4,5,6],[10,8,-12]])
---
+++ Day-7/Readme.md
@@ -0,0 +1,5 @@
+diagonalDifference()
+Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+Question was gotten from hackeRank website. Link to questions below.
+https://www.hackerrank.com/interview/preparation-kits/one-month-preparation-kit/one-month-week-one/challenges
