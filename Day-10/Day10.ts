6a42c4f0f41a4c213c37383eb67d16edff70c4f0
Beri28 <cletusberinyuy@gmail.com>
2025-01-15 22:17:05 +0100

    updated code to handle some other test cases

--- Day-10/Day10.ts
+++ Day-10/Day10.ts
@@ -1,7 +1,8 @@
 const twoArrays=(a:number[],b:number[],k:number):string=>{
-    let tempArray:number[]=a
+    let tempArray:number[]=b
     for(let i=0;i<b.length;i++){
-        let temp:number=k-b[i]
+        let temp:number=k-a[i]
+        // console.log(tempArray)
         if(tempArray.includes(temp)){
             let location:number=tempArray.indexOf(temp)
             tempArray=tempArray.filter((item,index)=>{
@@ -9,10 +10,20 @@ const twoArrays=(a:number[],b:number[],k:number):string=>{
             })
         }
         else{
+            console.log(k,a[i],temp,tempArray)
+            if(k!==a[i]){
+                console.log('NO')
                 return "NO"
+            }
         } 
     }
+    console.log("YES")
     return "YES"
 }
-console.log(twoArrays([2, 1, 3],[7, 8, 9],10))
-console.log(twoArrays([1, 2, 2, 1],[3, 3, 3, 4],5))
+// console.log(twoArrays([2, 1, 3],[7, 8, 9],10))
+// console.log(twoArrays([1, 2, 2, 1],[3, 3, 3, 4],5))
+// console.log(twoArrays([1, 3],[3,1],4))
+// console.log(twoArrays([2,3,1,1,1],[1, 3, 4, 3 ,3],5))
+// console.log(twoArrays([1 ,5 ,1 ,4 ,4 ,2 ,7 ,1 ,2 ,2],[8 ,7 ,1 ,7 ,7 ,4 ,4 ,3 ,6 ,7],9))
+console.log(twoArrays([3 ,6 ,8 ,5 ,9 ,9 ,4 ,8 ,4 ,7],[5 ,1 ,0 ,1 ,6 ,4 ,1 ,7 ,4 ,3],9))
+// console.log(twoArrays([4 ,4 ,3 ,2 ,1 ,4 ,4 ,3 ,2 ,4],[2 ,3 ,0 ,1 ,1 ,3 ,1 ,0 ,0 ,2],4))
