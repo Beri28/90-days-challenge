68fcc00f5c670777f102a7e7b6d47c741265726a
Beri28 <cletusberinyuy@gmail.com>
2025-01-15 21:19:05 +0100

    day 10 exercise

---
+++ Day-10/Day10.ts
@@ -0,0 +1,31 @@
+const twoArrays=(a:number[],b:number[],k:number):string=>{
+    let tempArray:number[]=a
+    for(let i=0;i<b.length;i++){
+        let temp:number=k-b[i]
+        if(tempArray.includes(temp)){
+            let location:number=tempArray.indexOf(temp)
+            tempArray=tempArray.filter((item,index)=>{
+                if(index!==location) return item
+            })
+        }
+        else{
+                return "No"
+        } 
+    }
+    // b.forEach((num,index)=>{
+    //     console.log(tempArray)
+    //     let temp:number=k-num
+    //     if(tempArray.includes(temp)){
+    //     let location:number=tempArray.indexOf(temp)
+    //     tempArray=tempArray.filter((item,index)=>{
+    //         if(index!==location) return item
+    //     })
+    //     }
+    //     else{
+    //          return "No"
+    //     }
+    // })
+    return "Yes"
+}
+console.log(twoArrays([2, 1, 3],[7, 8, 9],10))
+console.log(twoArrays([1, 2, 2, 1],[3, 3, 3, 4],5))
