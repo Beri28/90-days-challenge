669ca103ecf7024f6dbc0f079008cd5266379c92
Beri28 <cletusberinyuy@gmail.com>
2025-01-19 19:54:51 +0100

    updated typescript code to handle all test cases and updated readme files

--- Day-14/Day14.ts
+++ Day-14/Day14.ts
@@ -1,16 +1,19 @@
-const caesarCipher=(textString:string,rotator:number)=>{
+const caesarCipher=(textString:string,rotator:number):string=>{
     // textString=textString.toLowerCase()
     let newString:string[]=textString.split('')//.join("")
-    console.log(newString)
     const alphabet:string='abcdefghijklmnopqrstuvwxyz'
     const alphabet2:string=alphabet.toUpperCase()
     for(let i=0;i<textString.length;i++){
-        if(newString[i]===newString[i].toUpperCase()){
-            newString[i]=alphabet[alphabet2.indexOf(newString[i])+rotator]
-        }else{
-            newString[i]=alphabet[alphabet.indexOf(newString[i])+rotator] 
+        if(!alphabet.includes(newString[i]) && !alphabet2.includes(newString[i])){
+            newString[i]=newString[i]
+        }
+        else if(newString[i]===newString[i].toUpperCase()){
+            newString[i]=alphabet2[(alphabet2.indexOf(newString[i])+rotator)%26]
+        }else if(newString[i]===newString[i].toLowerCase()){
+            newString[i]=alphabet[(alphabet.indexOf(newString[i])+rotator)%26] 
         }
     }
     console.log(newString.join(""))
+    return newString.join("")
 }
 caesarCipher("There's a stairman in the sky",3)
---
+++ Day-14/Readme.md
@@ -0,0 +1,7 @@
+caesarCipher()
+Julius Caesar protected his confidential information by encrypting it using a cipher. Caesar's cipher shifts each letter by a number of letters. If the shift takes you past the end of the alphabet, just rotate back to the front of the alphabet. In the case of a rotation by 3, w, x, y and z would map to z, a, b and c.
+
+Code in c file doesn't pass all test cases, it passes very view cases. So decided to write the function in typescript and it worked and passed all test cases. Initially wanted to use c because of easy manipulation of characters(like incrementing a character 'a' to obtain, like 'a'++ but ran into problems like making the code pass all test cases)
+
+Question was gotten from hackeRank website. Link to questions below.
+https://www.hackerrank.com/interview/preparation-kits/one-month-preparation-kit/one-month-week-two/challenges
