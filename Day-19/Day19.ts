226335a356879e74c385d79f5fdf55aa35c26455
Beri28 <cletusberinyuy@gmail.com>
2025-01-25 05:03:32 +0100

    optimizing code of day 19

--- Day-19/Day19.ts
+++ Day-19/Day19.ts
@@ -12,13 +12,25 @@ const gridChallenge =(grid:string[]):string=>{
             }
         }
         grid[i]=gridLetters.join("")
-        if(i<grid.length-1 && alphabet.indexOf(grid[i][i])>alphabet.indexOf(grid[i+1][i])){
-            console.log("NO")
-            return "NO"
+        console.log(grid[i])
+        // console.log(alphabet.indexOf(grid[i][0]),alphabet.indexOf(grid[i+1][0]))
+        if(i<grid.length-1){
+            if(alphabet.indexOf(grid[i][0])>alphabet.indexOf(grid[i+1][0])){
+                console.log("NO")
+                return "NO"
+            }
         }
     }
+    
     console.log("YES")
     return "YES"
 }
-gridChallenge(['ebacd', 'fghij', 'olmkn', 'trpqs', 'xywuv'])
-gridChallenge(['abc', 'ade', 'efg'])
+// gridChallenge(['ebacd', 'fghij', 'olmkn', 'trpqs', 'xywuv'])
+// gridChallenge(['abc', 'ade', 'efg'])
+// gridChallenge(['sur',
+//     'eyy',
+//     'gxy'])
+gridChallenge(['vpvv',
+    'pvvv',
+    'vzzp',
+    'zzyy'])
