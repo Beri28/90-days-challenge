3bfb27b2c7875755c95982f752e43a40e98e7b74
Beri28 <cletusberinyuy@gmail.com>
2025-02-02 11:47:23 +0100

    exercise 3 of day 28

--- Day-28/Day28.ts
+++ Day-28/Day28.ts
@@ -7,4 +7,25 @@ function diagonalDifference(arr:number[][]):number{
     }
     return Math.abs(sumD1-sumD2)
 }
-console.log(diagonalDifference([[1,2,3],[4,5,6],[9,8,9]]))
+//console.log(diagonalDifference([[1,2,3],[4,5,6],[9,8,9]]))
+
+function miniMaxSum (arr:number[]){
+    let min:number=0
+    let max:number=0
+    for(let i=0;i<arr.length;i++){
+        for(let j=i+1;j<arr.length;j++){
+            if(arr[i]>arr[j]){
+                let temp=arr[i]
+                arr[i]=arr[j]
+                arr[j]=temp
+            }
+        }
+    }
+    for(let i=0;i<4;i++){
+        min+=arr[i]
+        max+=arr[i+1]
+    }
+    console.log(min,max)
+}
+
+miniMaxSum([7 ,69 ,2 ,221 ,8974])
