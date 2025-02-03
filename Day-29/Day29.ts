1138a8f7861f758409ce233c0fa889dbd6e9de04
Beri28 <cletusberinyuy@gmail.com>
2025-02-03 02:04:52 +0100

    day 29 exercises

---
+++ Day-29/Day29.ts
@@ -0,0 +1,38 @@
+function simpleArraySum(arr:number[]):number{
+    let sum:number=0
+    sum=arr.reduce((acc,curr)=>acc+curr,sum)
+    console.log(sum)
+    return sum
+}
+//simpleArraySum([1,2,3,4,5]) //Output: 15
+//simpleArraySum([1, 2 ,3, 4 ,10 ,11]) //Output: 31
+
+function countApplesAndOranges(s: number, t: number, a: number, b: number, apples: number[], oranges: number[]){
+    let aps:number=0
+    let ors:number=0
+    for(let i=0;i<apples.length;i++){
+        if(apples[i]+a>=s && apples[i]+a<=t){
+            aps++
+        }
+    }
+    for(let i=0;i<oranges.length;i++){
+        if(oranges[i]+b>=s && oranges[i]+b<=t){
+            ors++
+        }
+    }
+    console.log(aps)
+    console.log(ors)
+}
+// countApplesAndOranges(7,11,5,15,[-2,2,1],[5,-6]) //Output: 1 1
+// countApplesAndOranges(2,3,1,5,[2],[-2]) //Output: 1 1
+// countApplesAndOranges(7,10,4,12,[2,3,-4],[3,-2,-4]) //Output: 1 2
+
+function getTotalX (a: number[], b: number[]):number{
+    let between:number=0
+    for(let i=0;i<a.length;i++){
+        if(a.every((val)=>a[i]%val==0) && b.every((val)=>val%i==0)){
+            between++
+        }
+    }
+    return between
+}
---
+++ Day-29/Readme.md
@@ -0,0 +1,5 @@
+simpleArraySum()
+Sum elements of an array an return the sum. Applied the array.reduce((acc,curr)=>acc+curr,0) method here
+
+countApplesAndOranges()
+Link to question https://www.hackerrank.com/challenges/apple-and-orange/problem?isFullScreen=true
