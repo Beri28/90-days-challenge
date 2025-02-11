fe23a705b1c75a9b2f237662513f1ac8ac63c7f2
Beri28 <cletusberinyuy@gmail.com>
2025-02-11 10:19:26 +0100

    exercise 2 of day 37

--- Day-37/Day37.ts
+++ Day-37/Day37.ts
@@ -10,5 +10,23 @@ function designerPdfViewer (h: number[], word: string):number{
     console.log("Area:",width*height)
     return width*height
 }
-designerPdfViewer([1, 3, 1,3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7],'zaba')
-designerPdfViewer([1, 3, 1, 3, 1, 4 ,1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],'abc')
+// designerPdfViewer([1, 3, 1,3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7],'zaba')
+// designerPdfViewer([1, 3, 1, 3, 1, 4 ,1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],'abc')
+
+function utopianTree(n: number): number {
+    // Write your code here
+    let growth:number=0
+    for(let i=0;i<=n;i++){
+        if(i%2===0){
+            growth++
+        }else{
+            growth=2*growth
+        }
+    }
+    console.log(growth)
+    return growth
+}
+utopianTree(5)
+utopianTree(0)
+utopianTree(1)
+utopianTree(4)
