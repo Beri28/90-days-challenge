6843f04210977f19717ce6614974330219bbb53e
Beri28 <cletusberinyuy@gmail.com>
2025-02-20 02:07:50 +0100

    a litle bit optmized

--- Day-45/Day45.ts
+++ Day-45/Day45.ts
@@ -1,12 +1,20 @@
 function squares(a: number, b: number): number {
     let count:number=0
-    for(let i=a;i<=b;i++){
-        let sqrt=Math.sqrt(i)
-        if(Math.floor(sqrt)===sqrt) count++
+    if(a>b) return 0
+    if(a===b){
+        if((Math.floor(Math.sqrt(a))===Math.sqrt(a))) return 1
+        return 0
     }
-    console.log(count)
-    return count
+    let mid=Math.floor((b+a)/2)
+    return squares(a,mid)+squares(mid+1,b)
 }
-squares(24,49)
-squares(3,9)
-squares(17,24)
+console.log(squares(178809602, 330598161))
+console.log(squares(284829997, 636760792))
+console.log(squares(344808647, 444776424))
+// 178809602 330598161
+// 284829997 636760792
+// 344808647 444776424
+// 100052345 224762469
+// 135964528 323753587
+
+
