c9db327861ddb88f0e4a6afa5a0b4207197e9716
Beri28 <cletusberinyuy@gmail.com>
2025-02-21 05:15:11 +0100

    optimized code

--- Day-47/Day47.ts
+++ Day-47/Day47.ts
@@ -1,14 +1,11 @@
 function circularArrayRotation(a: number[], k: number, queries: number[]): number[] {
     // Write your code here
     let newPos:number[]=[]
-    queries.forEach(pos=>{
-        newPos.push(a[(((a.length-k)+pos)%a.length)])
+    queries.map(pos=>{
+        newPos.push(a.at(((a.length-k)+pos)%a.length)|| 0)
     })
-    // newPos.forEach((pos)=>{
-    //     console.log(a[pos])
-    // })
-    console.log(newPos)
     return newPos
 }
-circularArrayRotation([3,4,5],2,[1,2])
-circularArrayRotation([1,2,3],2,[0,1,2])
+// function circularArrayRotation(a, k, queries) { return queries.map(q => a.at((q - k) % a.length)) }
+console.log(circularArrayRotation([3,4,5],2,[1,2]))
+console.log(circularArrayRotation([1,2,3],2,[0,1,2]))
