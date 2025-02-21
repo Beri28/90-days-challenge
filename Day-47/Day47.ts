1473156ff55f37ba9c18ed97ccbea5af48f00d4b
Beri28 <cletusberinyuy@gmail.com>
2025-02-21 05:16:57 +0100

    minor adjustment

--- Day-47/Day47.ts
+++ Day-47/Day47.ts
@@ -2,10 +2,11 @@ function circularArrayRotation(a: number[], k: number, queries: number[]): numbe
     // Write your code here
     let newPos:number[]=[]
     queries.map(pos=>{
+        //made use of the  array at method that i saw in discussions room solution
         newPos.push(a.at(((a.length-k)+pos)%a.length)|| 0)
     })
     return newPos
 }
-// function circularArrayRotation(a, k, queries) { return queries.map(q => a.at((q - k) % a.length)) }
+// function circularArrayRotation(a, k, queries) { return queries.map(q => a.at((q - k) % a.length)) } //from discussions room, it works
 console.log(circularArrayRotation([3,4,5],2,[1,2]))
 console.log(circularArrayRotation([1,2,3],2,[0,1,2]))
