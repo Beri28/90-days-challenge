eb3244f671aaaa462cf7d27110020e9fd88cf922
Beri28 <cletusberinyuy@gmail.com>
2025-02-26 04:06:59 +0100

    more

--- Day-52/Day52.ts
+++ Day-52/Day52.ts
@@ -1,4 +1,4 @@
-const createCounter = function(n) {
+const createCounter1 = function(n) {
     
     return function() {
         return n++
@@ -29,5 +29,30 @@ let expect = function(val) {
         notToBe
     }
 };
-console.log(expect(5).toBe(5))
-console.log(expect(5).toBe(null))
+// console.log(expect(5).toBe(5))
+// console.log(expect(5).toBe(null))
+
+function createCounter(init){
+    let old=init
+    function increment(){
+        old+=1
+        return old 
+    }
+    function decrement(){
+        old-=1
+        return old
+    }
+    function reset(){
+        old=init
+        return old
+    }
+    return {
+       increment,
+       decrement,
+       reset
+    }
+}
+const counter = createCounter(5);
+console.log(counter.increment()); // 6
+console.log(counter.reset()); // 5
+console.log(counter.decrement()); // 4
