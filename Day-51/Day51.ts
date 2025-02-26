6714c408e7b2fd72f08e214df9e36e13a509d49b
Beri28 <cletusberinyuy@gmail.com>
2025-02-26 01:18:22 +0100

    fix attempt

--- Day-51/Day51.ts
+++ Day-51/Day51.ts
@@ -1,19 +1,22 @@
 function howManyGames(p:number,d:number,m:number,s:number): number {
     // Write your code here
-    s-=p
+    if(p===m) return s/p
     let games=1
-    while(s>0){
-        console.log("Before:",s)
+    s-=p
+    while(s>=m){
         if(p<=m){
             s-=m
+            // }
         }else{
             p=p-d
             s-=p
         }
-        console.log("After:",s)
         games++
     }
     console.log(games)
     return games
 }
 howManyGames(20,3,6,70)
+howManyGames(20 ,3 ,6 ,85)
+howManyGames(20 ,3 ,6 ,80)
+howManyGames(1 ,100 ,1 ,9777)
