9559a979556e95e89f6eca43d1fe5aa89c94b759
Beri28 <cletusberinyuy@gmail.com>
2025-03-08 23:12:22 +0100

    still day 61 changes

--- Day-61/Day61.ts
+++ Day-61/Day61.ts
@@ -15,14 +15,18 @@ function missingNumbers(arr: number[], brr: number[]): number[] {
         if(!arr.includes(num) && !indexes.includes(num)){
             indexes.push(num)
         }
+        // if(arr.includes(num)){
+        //     arr=index===0?arr.slice(1):[...arr.slice(0,index),...arr.slice(index+1)]
+        // }
         if(already.includes(num) && !indexes.includes(num)){
             if(checkMultiApp(arr,num)!==checkMultiApp(brr,num)){
                 indexes.push(num)
             }
         }
-        already.push(num)
+        !already.includes(num) && already.push(num)
 
     })
+    // console.log(already)
     return indexes.sort((a,b)=>a-b)
 }
 console.log(missingNumbers([203 ,204 ,205 ,206 ,207 ,208 ,203 ,204 ,205 ,206],[203 ,204 ,204 ,205 ,206 ,207 ,205 ,208 ,203 ,206 ,205 ,206 ,204]))
