8502b38e38980cac3beaad797f0d0d143a7d150e
Beri28 <cletusberinyuy@gmail.com>
2025-03-12 23:34:01 +0100

    day 66

---
+++ Day-66/Day66.ts
@@ -0,0 +1,26 @@
+function anagram(s: string): number {
+    // Write your code here
+    if(s.length%2!==0) return -1
+    if(s.length===2) return 1
+    let changes=0
+    let left=s.slice(0,(s.length/2))
+    let right=s.slice((s.length/2))
+    let rArray:any=right.split('')
+    if(left===right) return 0
+    if(left.split('').every((l)=>right.includes(l))) return 0
+    for(let i=0;i<right.length;i++){
+        if(!rArray.includes(left[i])) changes++
+        rArray[i]=undefined
+    }
+    // console.log("Left:",left)
+    // console.log("Right:",right)
+    return changes
+}
+
+// console.log(anagram('aaabbb'))
+// console.log(anagram('ab'))
+// console.log(anagram('abc'))
+// console.log(anagram('mnop'))
+// console.log(anagram('xyyx'))
+// console.log(anagram('xaxbbbxx'))
+console.log(anagram('fdhlvosfpafhalll'))
