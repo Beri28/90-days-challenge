240bd9cc89dafc267f301e127982d0c6b835f212
Beri28 <cletusberinyuy@gmail.com>
2025-03-19 20:53:55 +0100

    optimized code

--- Day-73/Day73.ts
+++ Day-73/Day73.ts
@@ -1,4 +1,5 @@
 function BalancedBrackets(s:string):string{
+    let stack:string[]=[]
     if(s.includes('(') && !s.includes(')')) return "NO"
     if(s.indexOf('(') > s.indexOf(')')) return "NO"
     if(s.includes('{') && !s.includes('}')) return "NO"
@@ -7,50 +8,24 @@ function BalancedBrackets(s:string):string{
     if(s.indexOf('[') > s.indexOf(']')) return "NO"
     for(let i=0;i<s.length;i++){
         if(s[i]==='('){
-            let temp=s.slice(i+1,s.lastIndexOf(')'))
-            if(temp.length===0) continue
-            return BalancedBrackets(temp)
+            stack.push(s[i])
         }
         else if(s[i]==='{'){
-            let temp=s.slice(i+1,s.lastIndexOf('}'))
-            if(temp.length===0) continue
-            return BalancedBrackets(temp)
+            stack.push(s[i])
         }
         else if(s[i]==='['){
-            let temp=s.slice(i+1,s.lastIndexOf(']'))
-            if(temp.length===0) continue
-            return BalancedBrackets(temp)
+            stack.push(s[i])
+        }
+        else {
+            if(s[i]===')' && stack.pop()!=='(') return "NO"
+            if(s[i]==='}' && stack.pop()!=='{') return "NO"
+            if(s[i]===']' && stack.pop()!=='[') return "NO"
         }
     }
-    // if(s[0]==='('){
-    //     if(s[s.length-1]===')'){
-    //         let temp=s.slice(1,s.lastIndexOf(')'))
-    //         BalancedBrackets(temp)
-    //     }else{
-    //         return "NO"
-    //     }
-    // }
-    // if(s[0]==='{'){
-    //     if(s[s.length-1]==='}'){
-    //         let temp=s.slice(1,s.lastIndexOf('}'))
-    //         BalancedBrackets(temp)
-    //     }else{
-    //         return "NO"
-    //     }
-    // }
-    // if(s[0]==='['){
-    //     if(s[s.length-1]===']'){
-    //         let temp=s.slice(1,s.lastIndexOf(']'))
-    //         BalancedBrackets(temp)
-    //     }else{
-    //         return "NO"
-    //     }
-    // }
     return "YES"
 }
 
 console.log(BalancedBrackets('{[()]}'))
 console.log(BalancedBrackets('{[(])}'))
 console.log(BalancedBrackets('{{[[(())]]}}'))
-console.log(BalancedBrackets('{}'))
-console.log(BalancedBrackets('{{{{'))
+console.log(BalancedBrackets('{(([])[])[]]}'))
