7201f7fa76084cde55fffc79a5d2315083f5457f
Beri28 <cletusberinyuy@gmail.com>
2025-03-30 23:28:30 +0100

    day 84 exercise, incomplete

---
+++ Day-84/Day84.ts
@@ -0,0 +1,28 @@
+type Fn<T> = () => Promise<T>
+
+function promiseAll<T>(functions: Fn<T>[]): Promise<T[]> {
+    let res:any[]=[]
+    return new Promise((resolve,reject)=>{
+        for(let i=0;i<functions.length;i++){
+            // res.push(functions[i]())
+            functions[i]().then((resl)=>{
+                console.log("Resolve for each",resl)
+                res.push(resl)
+                console.log(res)
+                // if(i===functions.length-1) resolve(res)
+            }).catch((rej)=>{
+                reject(rej)
+            }) 
+        }
+        // console.log("//////////")
+        // console.log(res)
+        // resolve(res)
+    })
+};
+
+const promise = promiseAll([
+    () => new Promise(resolve => setTimeout(() => resolve(4), 50)), 
+    () => new Promise(resolve => setTimeout(() => resolve(10), 150)), 
+    () => new Promise(resolve => setTimeout(() => resolve(16), 100))
+])
+promise.then(console.log); // [42]
