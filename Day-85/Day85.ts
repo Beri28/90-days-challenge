07c7286fc88bf6588363d0d6a21c689761ab4bc2
Beri28 <cletusberinyuy@gmail.com>
2025-03-31 17:02:09 +0100

    fix

--- Day-85/Day85.ts
+++ Day-85/Day85.ts
@@ -2,6 +2,9 @@ type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string
 type Obj = Record<string, JSONValue> | JSONValue[]
 
 function isEmpty(obj: Obj): boolean {
-    if(obj.length===0 || Object.keys(obj).length===0) return true
+    console.log(Object.values(obj).length)
+    if((Array.isArray(obj) && obj.length===0) || Object.values(obj).length===0) return true
     return false
 };
+console.log(isEmpty({"length": 0}))
+console.log(isEmpty([]))
